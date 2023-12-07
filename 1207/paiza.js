681701d40cbc54f8f4119566472df71f1687ec71
itc-s23018 <s23018@std.it-college.ac.jp>
2023-12-07 07:36:31 +0000

    1つのデータ入力" "

--- 1207/paiza.js
+++ 1207/paiza.js
@@ -1,8 +1,6 @@
-const print = console.log
 const stream = require('fs')
   .readFileSync('./data', 'utf8')
   .trim()
 const lines = stream.split('\n')
-const [a, b] = lines
 
-print(lines[0])
+console.log(lines[0])
