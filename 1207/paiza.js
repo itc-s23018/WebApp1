75764299e42ec335cf15f26877a310778a261b7a
itc-s23018 <s23018@std.it-college.ac.jp>
2023-12-07 13:50:07 +0000

    更新

--- 1207/paiza.js
+++
@@ -1,8 +0,0 @@
-const print = console.log
-const stream = require('fs')
-  .readFileSync('./data', 'utf8')
-  .trim()
-const lines = stream.split('\n')
-const [S] = lines
-
-print(S)
--- 1207/paiza4.js
+++
@@ -1,8 +0,0 @@
-const print = console.log
-const stream = require('fs')
-  .readFileSync('/data', 'utf8')
-  .trim()
-const lines = stream.split('\n')
-
-const [N, ...rest] = lines
-print(rest.join('\n'))
